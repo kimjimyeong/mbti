761455263d5ad3ce0e4f559fd2518bb62ca075fa
kjm <81976170+kimjimyeong@users.noreply.github.com>
2024-05-19 20:40:21 +0900

    Delete src/main/java/com/example/mbti directory

--- src/main/java/com/example/mbti/repository/MbtiTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mbti.repository;
-
-import com.example.mbti.model.entity.MbtiType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface MbtiTypeRepository extends JpaRepository<MbtiType, Long> {
-}
